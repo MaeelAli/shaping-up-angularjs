6823f8aef55c7c0b009f38c60eacfc20d92f4390
Ismaeel <maeelali92@gmail.com>
2016-06-08 23:15:25 -0400

    L2: Gems ordered by price in descending order. Added thumbnail images using ng-src directive.

--- app.js
+++ app.js
@@ -14,6 +14,16 @@
 		name: 'Dodecahedron Gem',
 		price: 2.95,
 		description: 'Some gems havce hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems',
+		images: [
+			{
+				full: "./img/dodecahedron-01-full.jpg",
+				thumb: "./img/dodecahedron-01-thumb.jpg"
+			},
+			{
+				full: "./img/dodecahedron-02-full.jpg",
+				thumb: "./img/dodecahedron-02-thumb.jpg"
+			}
+		],
 		canPurchase: true,
 		soldOut: false
 	},
@@ -21,6 +31,12 @@
 		name: "Pentagonal Gem",
 		price: 5.95,
 		description: "...",
+		images: [
+			{
+				full: "./img/pentagonal-full.jpg",
+				thumb: "./img/pentagonal-thumb.jpg"
+			}
+		],
 		canPurchase: true,
 		soldOut: false
 	}
