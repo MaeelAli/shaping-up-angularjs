922f98eabe919a09a80b3d45e8e1f7a8e09b966f
Ismaeel <maeelali92@gmail.com>
2016-06-15 23:39:07 -0400

    L4: Added custom directive product-panels to organize html and also link controller to directive.

--- app.js
+++ app.js
@@ -50,6 +50,28 @@
 		};
 	});
 
+	app.directive('productPanels', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-panels.html',
+			controller: function() {
+				//init tab to 1
+				this.tab = 1;
+
+				//function to set tab property to selected tab
+				this.selectTab = function(setTab) {
+					this.tab = setTab;
+				};
+
+				//returns true or false if tab is selected or not
+				this.isSelected = function(checkTab){
+					return this.tab == checkTab;
+				};
+			},
+			controllerAs: 'panel'
+		};
+	});
+
 	var gems = [ 
 	{
 		name: 'Dodecahedron Gem',
